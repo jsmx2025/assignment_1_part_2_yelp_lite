cabfe645a56b9e56dac4b3aa5063960fb786bf27
Raghu Betina <raghu@firstdraft.com>
2017-01-10 02:55:28 +0000

    Whitelist columns in UserId for ActiveAdmin

--- app/admin/user_id.rb
+++ app/admin/user_id.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register UserId do
 
+ permit_params :reviews_id, :email, :password
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
